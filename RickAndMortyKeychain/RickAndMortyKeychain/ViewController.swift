4bae1ff5eebd361c950f09ac0832caaf25afc024
ronesta <gvozzzd123@gmail.com>
2024-11-01 19:14:24 +0300

    Initial commit

---
+++ RickAndMortyKeychain/RickAndMortyKeychain/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  RickAndMortyKeychain
+//
+//  Created by Ибрагим Габибли on 01.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+}
